ef3351f9daa2ac8a649dcff8b3a0d0182e08593d
Chandan-6 <khamitkarsaichandan1035@gmail.com>
2024-09-03 18:40:57 +0530

    Added Research Publication page
    Signed-off-by: Chandan-6 <khamitkarsaichandan1035@gmail.com>
    
    * path /research-publications

---
+++ src/app/research-publications/PublicationCard.tsx
@@ -0,0 +1,44 @@
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import StarIcon from '@mui/icons-material/Star';
+
+export default function PublicationCard(props: { title: string; attribute: string, description: string, rating: number }) {
+
+    const ratingStars = [];
+    for (let i = 0; i < 5; i++) {
+        if (i < props.rating) {
+            ratingStars.push(<StarIcon key={i} style={{ color: 'gold' }} />);
+        } else {
+            ratingStars.push(<StarBorderIcon key={i} style={{ color: 'grey' }} />);
+        }
+    }
+    ratingStars.reverse();
+
+    return (
+        <div className="bg-[var(--secondary-color)] p-6 w-full rounded-3xl flex-col items-center justify-between space-y-8">
+            <div className="flex justify-between items-center">
+
+                <div>
+                    <p className="text-base uppercase font-bold">{props.title}</p>
+                    <p className="text-sm">{props.attribute}</p>
+                </div>
+
+                <div className="flex justify-end items-center space-x-2">
+                    <button className="text-[var(--main-color)] border rounded-3xl border-[var(--main-color)] px-8 py-2 text-sm font-medium">
+                        View
+                    </button>
+                    <button className="bg-[var(--main-color)] border rounded-3xl text-white px-8 py-2 text-sm font-medium">
+                        Save
+                    </button>
+                </div>
+            </div>
+
+            <p className="text-sm">
+                {props.description}
+            </p>
+
+            <div className="flex flex-row-reverse">
+                {ratingStars}
+            </div>
+        </div>
+    );
+}
---
+++ src/app/research-publications/page.tsx
@@ -0,0 +1,72 @@
+import Sidebar from "@/components/Sidebar";
+import AvatarImage from "@/components/AvatarImage";
+import PublicationCard from "./PublicationCard";
+import Footer from "@/components/Footer";
+
+const ResearchData = [
+  {
+    "attribute" : "Jhon Wick",
+    "title" : "speech recognition",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsu",
+    "rating" : 5
+  },
+  {
+    "attribute" : "Jhon Wick",
+    "title" : "speech recognition",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsu",
+    "rating" : 1
+  },
+  {
+    "attribute" : "Jhon Wick",
+    "title" : "speech recognition",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsu",
+    "rating" : 0
+  },
+  {
+    "attribute" : "Jhon Wick",
+    "title" : "speech recognition",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsu",
+    "rating" : 2
+  },
+  {
+    "attribute" : "Jhon Wick",
+    "title" : "speech recognition",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsu",
+    "rating" : 3
+  },
+  {
+    "attribute" : "Jhon Wick",
+    "title" : "speech recognition",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsu",
+    "rating" : 1
+  },
+  {
+    "attribute" : "Jhon Wick",
+    "title" : "speech recognition",
+    "description" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor Lorem ipsu",
+    "rating" : 3
+  },
+]
+
+export default function Page() {
+  return (
+    <div className="flex relative">
+      <Sidebar />
+      <div className="pt-36 px-24 w-full">
+        <div className="flex space-x-4">
+          <div className="text-2xl font-semibold uppercase">
+            Research Publications
+          </div>
+        </div>
+        <div className="grid grid-cols-1 mt-10 mb-16 gap-8 w-full">
+          {
+            ResearchData.map((item,index) => (
+              <PublicationCard attribute={item.attribute} title={item.title} description={item.description} key={index} rating={item.rating} />
+            ))
+          }
+        </div>
+        <Footer/>
+      </div>
+    </div>
+  );
+}
