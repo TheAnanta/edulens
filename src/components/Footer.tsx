e46d86cc665bc46678ed96efd8e0490f691de124
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-09-03 18:35:40 +0530

    footer debugged
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,19 @@
 const SocialMedial = [
-    {"X" : "link"},
-    {"Instagram" : "link"},
-    {"YouTube" : "link"},
+  { X: "link" },
+  { Instagram: "link" },
+  { YouTube: "link" },
 ];
 
-export default function Footer(){
-    return(
-        <div className="absolute bottom-4 right-4 w-full text-right flex justify-end items-center text-sm space-x-3">
-            {
-                SocialMedial.map((item, index) => (
-                    <p className="text-gray-500">{Object.keys(item)[0]}</p>
-                ))
-            }
+export default function Footer() {
+  return (
+    <div className="absolute bottom-4 right-4 w-full text-right flex justify-end items-center text-sm space-x-3">
+      {SocialMedial.map((item, index) => (
+        <p key={index} className="text-gray-500">{Object.keys(item)[0]}</p>
+      ))}
 
-            <p className="text-black">© GITAM (Deemed to Be University) 2024. All Rights Reserved</p>
-        </div>
-    )
-};
-
-
+      <p className="text-black">
+        © GITAM (Deemed to Be University) 2024. All Rights Reserved
+      </p>
+    </div>
+  );
+}
