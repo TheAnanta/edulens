9e454a17a014ee53807486283e91992bc08ff772
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-09-04 11:52:15 +0530

    yes2
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,10 @@
-export default function Card(props: { title: string; attribute: { [key: string]: any } }) {
+export default function Card(props: { title: string; attribute: any }) {
   return (
     <div className="bg-[var(--secondary-color)] p-6 w-full rounded-3xl flex items-center justify-between space-x-6">
-      <div className="flex flex-col items-center justify-center w-16 h-16 rounded-full bg-[var(--main-color)] text-white">{props.title}</div>
-      <div>
-         {/* Display the attributes (you'll need to decide how to format them) */}
-         {Object.entries(props.attribute).map(([key, value]) => (
-            <div key={key}>
-              <span>{key}: </span>
-              <span>{value}</span>
-            </div>
-          ))}
+      <div className="flex flex-col items-center justify-center w-16 h-16 rounded-full bg-[var(--main-color)] text-white">
+        {props.title}
       </div>
+      <p>{props.attribute}</p>
     </div>
   );
 }
-
