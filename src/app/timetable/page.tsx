4b8d9943927d538a0e6ebbbb8eb62388ba063cc6
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-09-04 09:55:33 +0530

    attendance updated
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/app/timetable/page.tsx
+++ src/app/timetable/page.tsx
@@ -4,11 +4,11 @@ import React from 'react';
 function App() {
   const scheduleData = [
     { subjectCode: 'CSEN1001', subject: 'DBMS', section: 'A', roomNo: '501', timings: '10:00 - 11:00' },
-    { subjectCode: 'CSEN1001', subject: 'DBMS', section: 'A', roomNo: '501', timings: '10:00 - 11:00' },
-    { subjectCode: 'CSEN1001', subject: 'DBMS', section: 'A', roomNo: '501', timings: '10:00 - 11:00' },
-    { subjectCode: 'CSEN1001', subject: 'DBMS', section: 'A', roomNo: '501', timings: '10:00 - 11:00' },
-    { subjectCode: 'CSEN1001', subject: 'DBMS', section: 'A', roomNo: '501', timings: '10:00 - 11:00' },
-    { subjectCode: 'CSEN1001', subject: 'DBMS', section: 'A', roomNo: '501', timings: '10:00 - 11:00' },
+    { subjectCode: 'CSEN1002', subject: 'Software Engineering', section: 'H', roomNo: '338', timings: '11:00 - 12:00' },
+    { subjectCode: 'CSEN1003', subject: 'Algorithms', section: 'B', roomNo: '224', timings: '12:00 - 13:00' },
+    { subjectCode: 'CSEN1004', subject: 'OS', section: 'I', roomNo: '608A', timings: '14:00 - 15:00' },
+    { subjectCode: 'CSEN1005', subject: 'Computer Networks', section: 'H', roomNo: '501', timings: '15:00 - 16:00' },
+    
   ];
 
   return (
@@ -16,7 +16,7 @@ function App() {
       <Sidebar />
       <div className="w-full overflow-hidden rounded-lg shadow-lg mt-24 space-y-10">
       <p className="w-full text-center text-4xl font-bold">TimeTable</p>
-        <table className="w-full bg-blue-700 text-white">
+        <table className="w-full bg-[var(--main-color)] text-white">
           <thead>
             <tr>
               <th className="head">Subject Code</th>
@@ -28,7 +28,7 @@ function App() {
           </thead>
           <tbody>
             {scheduleData.map((item, index) => (
-              <tr key={index} className="even:bg-blue-700 odd:bg-blue-600">
+              <tr key={index} className="even:bg-[var(--secondary-color)] even:text-black odd:bg-[var(--main-color)]">
                 <td>{item.subjectCode}</td>
                 <td>{item.subject}</td>
                 <td>{item.section}</td>
