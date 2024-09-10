3f846d741d6b8548c9c142c825a68d6addfbc7b1
Chandan-6 <khamitkarsaichandan1035@gmail.com>
2024-09-10 21:05:53 +0530

    Changed the analytics table background color.
    Signed-off-by: Chandan-6 <khamitkarsaichandan1035@gmail.com>
    
    * Added new quatnary color code to global.css

--- src/app/analytics/page.jsx
+++ src/app/analytics/page.jsx
@@ -101,26 +101,26 @@ const UniversityAnalysis = () => {
               <table className="w-full text-left border-collapse my-8">
                 <thead>
                   <tr>
-                    <th className="border border-gray-300 px-4 py-2 bg-gray-100">
+                    <th className="border border-gray-300 px-4 py-2 bg-[var(--tertiary)]">
                       Category
                     </th>
-                    <th className="border border-gray-300 px-4 py-2 bg-gray-100">
+                    <th className="border border-gray-300 px-4 py-2 bg-[var(--tertiary)]">
                       Score
                     </th>
-                    <th className="border border-gray-300 px-4 py-2 bg-gray-100">
+                    <th className="border border-gray-300 px-4 py-2 bg-[var(--tertiary)]">
                       Strengths
                     </th>
-                    <th className="border border-gray-300 px-4 py-2 bg-gray-100">
+                    <th className="border border-gray-300 px-4 py-2 bg-[var(--tertiary)]">
                       Weaknesses
                     </th>
-                    <th className="border border-gray-300 px-4 py-2 bg-gray-100">
+                    <th className="border border-gray-300 px-4 py-2 bg-[var(--tertiary)]">
                       Recommendations
                     </th>
                   </tr>
                 </thead>
                 <tbody>
                   {Object.keys(data).map((key) => (
-                    <tr key={key} className="odd:bg-white even:bg-gray-50">
+                    <tr key={key} className="odd:bg-[var(--quatnary-color)] even:bg-gray-50">
                       <td className="border border-gray-300 px-4 py-2">
                         {key}
                       </td>
